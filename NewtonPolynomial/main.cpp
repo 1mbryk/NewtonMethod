d230c66c67190fb9bff0b378647c616100a6149a
1mbryk <95372025+1mbryk@users.noreply.github.com>
2022-10-30 22:03:51 +0300

    Initial Commit

---
+++ NewtonPolynomial/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  NewtonPolynomial
+//
+//  Created by Mацвей Пажытных on 30.10.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
