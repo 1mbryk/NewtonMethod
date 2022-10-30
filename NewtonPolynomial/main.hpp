6a7bdbd4b83c75f9b7b92dc0fe0179ffc00d3a74
1mbryk <95372025+1mbryk@users.noreply.github.com>
2022-10-31 02:16:53 +0300

    initial commit

---
+++ NewtonPolynomial/main.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include "Constants.hpp"
+#include "GaussMethod.hpp"
+#include "Function.hpp"
+#include "OtherFunctions.hpp"
+
+
